e0b38d581b92444d52e0598dff68cadce26ba701
Ali Fahed <alifahed67@gmail.com>
2022-08-23 23:33:26 +0800

    Add and style Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './styles/style.scss';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/_navbar.scss';
+
+function Navbar() {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="header-text-container">
+          <h1 className="header-text">CV Application</h1>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
