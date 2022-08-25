3a05a3bbf8d9452dfa1e056fdf3d27b10ade8bf9
Ali Fahed <alifahed67@gmail.com>
2022-08-25 22:31:24 +0800

    Comment cv-photo-container

--- src/components/CvHeader.js
+++ src/components/CvHeader.js
@@ -4,9 +4,14 @@ import '../styles/_cvHeader.scss';
 function CvHeader() {
   return (
     <div className="cv-header-container">
-      <div className="cv-photo-container">
-        <div className="cv-photo"></div>
-      </div>
+      {/* <div className="cv-photo-container">
+        <input
+          type="file"
+          className="upload-btn"
+          accept="image/png, image/jpg"
+        />
+        <img src="" alt="CV photo" className="cv-photo" />
+      </div> */}
 
       <div className="cv-header-text-container">
         <div className="cv-name">Ali Fahed</div>
