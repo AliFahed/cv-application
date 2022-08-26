ebb8170aac64f5017749c0fd18e6f68ddd26fc76
Ali Fahed <alifahed67@gmail.com>
2022-08-26 17:03:32 +0800

    Add CvHeader edit functionality

--- src/components/CvHeader.js
+++ src/components/CvHeader.js
@@ -1,25 +1,113 @@
 import React from 'react';
 import '../styles/_cvHeader.scss';
 
-function CvHeader() {
-  return (
-    <div className="cv-header-container">
-      {/* <div className="cv-photo-container">
-        <input
-          type="file"
-          className="upload-btn"
-          accept="image/png, image/jpg"
-        />
-        <img src="" alt="CV photo" className="cv-photo" />
-      </div> */}
+function CvHeader(props) {
+  // only show the edit button on hovering over the cv header container
 
-      <div className="cv-header-text-container">
-        <div className="cv-name">Ali Fahed</div>
-        <div className="cv-summary">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-          voluptate quos, aliquid magni animi id dolorem quaerat aliquam saepe
-          dignissimos nam deleniti eaque earum similique tempore ab. Distinctio,
-          voluptates necessitatibus.
+  // update cv header details from default
+  const [cvName, setCvName] = React.useState(props.name);
+  const [cvSummary, setCvSummary] = React.useState(props.summary);
+
+  function showHeaderForm() {
+    const headerFromContainer = document.querySelector(
+      '.header-form-container'
+    );
+    headerFromContainer.classList.add('show-header-form');
+    headerFromContainer.classList.remove('hide-header-from');
+
+    submitHeaderFrom();
+  }
+
+  function submitHeaderFrom() {
+    const from = document.querySelector('form');
+    from.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      editHeaderDetails();
+    });
+  }
+
+  function editHeaderDetails() {
+    const fullName = document.getElementById('fullName');
+    setCvName(fullName.value);
+
+    const summary = document.getElementById('summary');
+    setCvSummary(summary.value);
+  }
+
+  function hideHeaderForm() {
+    const headerFromContainer = document.querySelector(
+      '.header-form-container'
+    );
+    headerFromContainer.classList.add('hide-header-from');
+    headerFromContainer.classList.remove('show-header-from');
+  }
+
+  return (
+    <div>
+      <div className="btn-container">
+        <button className="edit-cv-header-btn" onClick={showHeaderForm}>
+          Edit
+        </button>
+      </div>
+
+      <div className="header-form-container">
+        <form action="#" className="form">
+          <div className="input-wrapper">
+            <label htmlFor="fullName" className="label">
+              Full Name
+            </label>
+            <input
+              type="text"
+              id="fullName"
+              className="cv-full-name"
+              placeholder="Full Name"
+              required
+            />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="summary" className="label">
+              Summary
+            </label>
+            <textarea
+              name="summary"
+              id="summary"
+              cols="45"
+              rows="10"
+              className="cv-summary"
+              placeholder="Write a Summary"
+              required
+            ></textarea>
+          </div>
+
+          <div className="button-container">
+            <button
+              type="button"
+              className="close-btn"
+              onClick={hideHeaderForm}
+            >
+              Close
+            </button>
+            <button type="submit" className="submit-btn">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="cv-header-container">
+        {/* <div className="cv-photo-container">
+          <input
+            type="file"
+            className="upload-btn"
+            accept="image/png, image/jpg"
+          />
+          <img src="" alt="CV photo" className="cv-photo" />
+        </div> */}
+        <div className="cv-header-text-container">
+          <div className="cv-name">{cvName}</div>
+          <div className="cv-summary">{cvSummary}</div>
         </div>
       </div>
     </div>
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,7 +6,13 @@ import CvMainContent from './CvMainContent';
 function MainContent() {
   return (
     <main className="cv-container">
-      <CvHeader />
+      <CvHeader
+        name="Ali Fahed"
+        summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+        voluptate quos, aliquid magni animi id dolorem quaerat aliquam saepe
+        dignissimos nam deleniti eaque earum similique tempore ab. Distinctio,
+        voluptates necessitatibus."
+      />
       <CvMainContent />
     </main>
   );
